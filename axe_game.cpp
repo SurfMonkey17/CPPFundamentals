1ff9c43e7c2374fbe949d67ea8557d6ae675989c
Sherry Fisher <sherryw17@gmail.com>
2025-01-05 12:20:39 -0600

    2.3 completed

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,5 +3,15 @@ int main()
 {
     int width;
     width = 350;
+
+    float root_beer;
+    root_beer = 1.99;
+
+    double cheese_burger;
+    cheese_burger = 5.99;
+
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+
     InitWindow(width, 200, "Sherry's Window");
 }
