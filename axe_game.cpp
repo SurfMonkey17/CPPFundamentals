1a206d04271d349118bd3cc8cf66dac0fc5c28f2
Sherry Fisher <sherryw17@gmail.com>
2025-01-20 14:51:15 -0600

    added function to close window

--- axe_game.cpp
+++ axe_game.cpp
@@ -5,7 +5,7 @@ int main()
     int width = 350;
     InitWindow(width, 200, "Sherry's Window");
 
-    while (true) // keeps window open infinitely
+    while (WindowShouldClose() == false) // keeps window open infinitely
     {
         BeginDrawing();
         ClearBackground(RED);
