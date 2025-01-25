ff6ff7485662be9ab963740fc08391d7e49945f1
Sherry Fisher <sherryw17@gmail.com>
2025-01-25 12:45:21 -0600

    added coordinates for collision detection

--- axe_game.cpp
+++ axe_game.cpp
@@ -7,13 +7,26 @@ int main()
     InitWindow(width, height, "Axe Game");
 
     // circle coordinates
-
     int circle_x{200};
     int circle_y{200};
+    int circle_radius{25};
+
+    // circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
 
     // axe coordinates
     int axe_x{400};
     int axe_y{0};
+    int axe_length{50};
+
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
 
     int direction{10};
 
@@ -26,19 +39,19 @@ int main()
 
         SetTargetFPS(60);
 
-        DrawCircle(circle_x, circle_y, 25, BLUE);
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
         // move axe
 
         axe_y += direction;
 
-        if (axe_y > 400 || axe_y < 0)
+        if (axe_y > height || axe_y < 0) // height checks against edge of window
         {
             direction = -direction;
         }
 
-        if (IsKeyDown(KEY_D) && circle_x < 350)
+        if (IsKeyDown(KEY_D) && circle_x < width) // checks against width of window
         {
             circle_x += 10;
         }
